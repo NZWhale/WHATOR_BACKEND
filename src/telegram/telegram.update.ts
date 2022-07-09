5cad3ed6a09ac0211686a62cfa9d83debf1dce02
Nikita Zverev <zverevn420@gmail.com>
2022-07-09 21:45:34 +0300

    feat(telegram): telegram bot now can send archives, that less than 2 gb

--- src/telegram/telegram.update.ts
+++ src/telegram/telegram.update.ts
@@ -1,5 +1,5 @@
-import { Update, Start, Hears } from 'nestjs-telegraf';
-import { Markup } from 'telegraf';
+import { Update, Start, Hears, Action } from 'nestjs-telegraf';
+import { Markup, Context } from 'telegraf';
 import { CustomLoggerService } from '../logger/custom-logger.service';
 import * as WebTorrent from 'webtorrent';
 
@@ -8,11 +8,16 @@ export class TelegramUpdate {
   private readonly client;
   private readonly fileExtension;
   private readonly AdmZip;
+  private lastMessages;
 
   constructor(private readonly logger: CustomLoggerService) {
     this.client = new WebTorrent();
     this.fileExtension = require('file-extension');
     this.AdmZip = require('adm-zip');
+    this.lastMessages = {
+      bot: null,
+      user: null,
+    };
 
     this.client.on('download', (bytes) => {
       console.log('just downloaded: ' + bytes);
@@ -23,150 +28,197 @@ export class TelegramUpdate {
   }
 
   @Start()
-  async startCommand(ctx: any) {
+  async startCommand(ctx: Context) {
+    this.lastMessages.bot = ctx.message.message_id + 1;
     await ctx.reply(
       "Hey bro. Just send me magnet link and I'll download it. Click help button to get more information",
-      Markup.keyboard([
-        ['Help', 'Media', 'Status'],
-        ['Registration', 'Remove data'],
-        ['Sing up', 'Sing in'],
-      ]).resize(),
+      Markup.inlineKeyboard(
+        [
+          {
+            text: 'Help',
+            callback_data: 'Help',
+          },
+          {
+            text: 'Media',
+            callback_data: 'Media',
+          },
+          {
+            text: 'Status',
+            callback_data: 'Status',
+          },
+          {
+            text: 'Registration',
+            callback_data: 'Registration',
+          },
+          {
+            text: 'Remove data',
+            callback_data: 'Remove data',
+          },
+          {
+            text: 'Sing up',
+            callback_data: 'Sing up',
+          },
+          {
+            text: 'Sing in',
+            callback_data: 'Sing in',
+          },
+        ],
+        { columns: 3 },
+      ),
     );
-    await ctx.deleteMessage(ctx.message.message_id - 1);
+    await ctx.deleteMessage(ctx.message.message_id);
   }
 
-  @Hears('Help')
-  async helpCommand(ctx: any) {
-    await ctx.deleteMessage();
-    await ctx.deleteMessage(ctx.message.message_id - 1);
-    await ctx.reply(
+  @Action('Help')
+  async helpCommand(ctx: Context) {
+    await this.replyWithText(
+      ctx,
       '• To start downloading send magnet link into the chat.\n' +
         '• You can check downloading progress. Click status button.\n' +
         '• You can use web interface to watch you files. Click registration button and follow the instructions.\n' +
         '• To save your media data your should authorize into the app. Click sign up button.\n' +
         '• You can restore your media data. Click sign in button.\n' +
         '• You can delete all you data like media and user data. Click remove data button.\n',
-      Markup.keyboard([
-        ['Help', 'Media', 'Status'],
-        ['Registration', 'Remove data'],
-        ['Sing up', 'Sing in'],
-      ]).resize(),
     );
   }
 
-  @Hears('Status')
-  async status(ctx: any) {
-    await ctx.deleteMessage();
-    await ctx.deleteMessage(ctx.message.message_id - 1);
+  @Action('Status')
+  async status(ctx: Context) {
     const progress = this.client.progress;
-    await ctx.reply(
-      `Downloading progress ${progress}`,
-      Markup.keyboard([
-        ['Help', 'Media', 'Status'],
-        ['Registration', 'Remove data'],
-        ['Sing up', 'Sing in'],
-      ]).resize(),
-    );
+    await this.replyWithText(ctx, `Downloading progress ${progress}`);
   }
 
-  @Hears('Registration')
-  async registration(ctx: any) {
-    await ctx.deleteMessage();
-    await ctx.deleteMessage(ctx.message.message_id - 1);
-    await ctx.reply(
-      'Registration link',
-      Markup.keyboard([
-        ['Help', 'Media', 'Status'],
-        ['Registration', 'Remove data'],
-        ['Sing up', 'Sing in'],
-      ]).resize(),
-    );
+  @Action('Registration')
+  async registration(ctx: Context) {
+    await this.replyWithText(ctx, 'Registration link');
   }
 
-  @Hears('Remove data')
-  async removeData(ctx: any) {
-    await ctx.deleteMessage();
-    await ctx.deleteMessage(ctx.message.message_id - 1);
-    await ctx.reply(
-      'Data was removed',
-      Markup.keyboard([
-        ['Help', 'Media', 'Status'],
-        ['Registration', 'Remove data'],
-        ['Sing up', 'Sing in'],
-      ]).resize(),
-    );
+  @Action('Remove data')
+  async removeData(ctx: Context) {
+    await this.replyWithText(ctx, 'Data was removed');
   }
 
-  @Hears('Sing up')
-  async signUp(ctx: any) {
-    await ctx.deleteMessage();
-    await ctx.deleteMessage(ctx.message.message_id - 1);
-    await ctx.reply(
-      'You are signed up',
-      Markup.keyboard([
-        ['Help', 'Media', 'Status'],
-        ['Registration', 'Remove data'],
-        ['Sing up', 'Sing in'],
-      ]).resize(),
-    );
+  @Action('Sing up')
+  async signUp(ctx: Context) {
+    await this.replyWithText(ctx, 'You are signed up');
   }
 
-  @Hears('Sing in')
-  async signIn(ctx: any) {
-    await ctx.deleteMessage();
-    await ctx.deleteMessage(ctx.message.message_id - 1);
-    await ctx.reply(
-      'You are signed in',
-      Markup.keyboard([
-        ['Help', 'Media', 'Status'],
-        ['Registration', 'Remove data'],
-        ['Sing up', 'Sing in'],
-      ]).resize(),
-    );
+  @Action('Sing in')
+  async signIn(ctx: Context) {
+    await this.replyWithText(ctx, 'You are signed in');
   }
 
-  @Hears('Media')
-  async media(ctx: any) {
-    await ctx.deleteMessage();
-    await ctx.deleteMessage(ctx.message.message_id - 1);
-    await ctx.reply(
-      'You are signed in',
-      Markup.keyboard([
-        ['Help', 'Media', 'Status'],
-        ['Registration', 'Remove data'],
-        ['Sing up', 'Sing in'],
-      ]).resize(),
-    );
+  @Action('Media')
+  async media(ctx: Context) {
+    const torrents = this.client.torrents.map((el) => el.name);
+    console.log(torrents);
+    await this.replyWithText(ctx, torrents);
+  }
+
+  async downloaded(ctx: Context, torrent: any) {
+    // await this.replyWithText(ctx, 'done');
+    await ctx.replyWithDocument({
+      source: `./${torrent.dn}.zip`,
+    });
+    await torrent.destroy();
   }
 
   @Hears(new RegExp(/magnet:?/g))
   async magentoUrl(ctx: any) {
     try {
-      this.client.add(ctx.message.text, (torrent) => {
-        const zip = new this.AdmZip();
-        torrent.on('done', function () {
-          console.log('torrent finished downloading');
-          torrent.files.forEach(function (file) {
-            zip.addLocalFile(file.path);
-          });
-          zip.writeZip(`./${torrent.dn}.zip`);
-          ctx
-            .replyWithDocument({
-              source: `./${torrent.dn}.zip`,
-            })
-            .then(() => {
-              console.log('file sent to telegram');
-              torrent.destroy();
-            })
-            .catch(function (error) {
-              console.log(error);
+      ctx.deleteMessage(ctx.message.message_id);
+      this.client.add(
+        ctx.message.text,
+        {
+          path: './download',
+          addUID: true,
+        },
+        async (torrent) => {
+          if (torrent.length > 2136746229) {
+            torrent.destroy();
+            await this.replyWithText(
+              ctx,
+              'The size of downloaded files must be less than 2 GB.\n' +
+                'To download files larger than 2 GB you should use web interface',
+            );
+            return;
+          }
+          await this.replyWithText(
+            ctx,
+            'Downloading will start in a few seconds',
+          );
+          const zip = new this.AdmZip();
+          torrent.on('done', async () => {
+            console.log('torrent finished downloading');
+            torrent.files.forEach((file) => {
+              zip.addLocalFile(file.path);
             });
-        });
-      });
+            zip.writeZip(`${torrent.dn}.zip`);
+            await this.downloaded(ctx, torrent);
+            // ctx
+            //   .replyWithDocument({
+            //     source: `./${torrent.dn}.zip`,
+            //   })
+            //   .then(() => {
+            //     console.log('file sent to telegram');
+            //     torrent.destroy();
+            //   })
+            //   .catch(function (error) {
+            //     console.log(error);
+            //   });
+            // console.log('file sent to telegram');
+            // torrent.destroy();
+          });
+        },
+      );
+    } catch (e) {
+      this.logger.error(e.message);
+    }
+  }
 
-      await ctx.deleteMessage();
-      await ctx.deleteMessage(ctx.message.message_id - 1);
-      await ctx.reply('Downloading will start in a few seconds');
+  async replyWithText(ctx: Context, text) {
+    try {
+      const messageId = this.lastMessages.bot;
+      const chatId = ctx.chat.id;
+      await ctx.telegram.editMessageText(
+        chatId,
+        messageId,
+        undefined,
+        text,
+        Markup.inlineKeyboard(
+          [
+            {
+              text: 'Help',
+              callback_data: 'Help',
+            },
+            {
+              text: 'Media',
+              callback_data: 'Media',
+            },
+            {
+              text: 'Status',
+              callback_data: 'Status',
+            },
+            {
+              text: 'Registration',
+              callback_data: 'Registration',
+            },
+            {
+              text: 'Remove data',
+              callback_data: 'Remove data',
+            },
+            {
+              text: 'Sing up',
+              callback_data: 'Sing up',
+            },
+            {
+              text: 'Sing in',
+              callback_data: 'Sing in',
+            },
+          ],
+          { columns: 3 },
+        ),
+      );
     } catch (e) {
       this.logger.error(e.message);
     }
